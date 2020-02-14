8beddbb85f5b710c7c93319ad67f4d299fabb6ae
Michael Sui <msuibusiness@gmail.com>
2020-02-14 03:06:37 -0500

    finished initial front end ui design.

--- FrontEnd/frnt/src/app/components/register/register.component.ts
+++ FrontEnd/frnt/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatToolbarModule, MatButtonModule, MatSlideToggle} from '@angular/material';
+
 
 @Component({
   selector: 'app-register',
