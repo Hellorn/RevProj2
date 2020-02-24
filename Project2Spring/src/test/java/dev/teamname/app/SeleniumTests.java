6ac5e2890387da2af86fc931f13bbaa7f7a6914b
Sarah Dellheim <sarahdellheim.0xff@gmail.com>
2020-02-24 08:45:45 -0500

    Updated tests!

--- Project2Spring/src/test/java/dev/teamname/app/SeleniumTests.java
+++ Project2Spring/src/test/java/dev/teamname/app/SeleniumTests.java
@@ -36,71 +36,231 @@ class SeleniumTests {
 		// Create a driver and start automating stuff
 		driver = new ChromeDriver();
 	}
-	
-//	@Test
-	@Order(1)
-	void ClickUserLogin() throws InterruptedException {
+	@Test
+	@Order(9)
+	void regRestaurant() throws InterruptedException {
 		driver.get("localhost:4200/");
-//	@FindBy(css="div[lang='en']")
-		WebElement loginBtn = driver.findElement(By.cssSelector("button.log"));
-		loginBtn.click();
-		Thread.sleep(4000);
+		// Click "Sign up"
+		WebElement managerLogin = driver.findElement(By.xpath("//span[contains(.,'Sign Up')]"));
+		managerLogin.click();
+		Thread.sleep(1000);
+
+		// Register as a restaurant
+		WebElement username = driver.findElement(By.id("username"));
+		WebElement password = driver.findElement(By.id("password"));
+		WebElement checkbox = driver.findElement(By.className("mat-checkbox"));
+		WebElement submitBtn = driver.findElement(By.cssSelector("button.log"));
+		username.sendKeys("Sonic");
+		password.sendKeys("gottagofast");
+		checkbox.click();
+		submitBtn.click();
+
+		Thread.sleep(2000);
 	}
 
-	static void loginUser() throws InterruptedException {
-		driver.get("localhost:4200/");
+	@Test
+	@Order(10)
+	void loginNewRest() throws InterruptedException {
 
 		WebElement username = driver.findElement(By.id("username"));
 		WebElement password = driver.findElement(By.id("password"));
 		WebElement loginBtn = driver.findElement(By.cssSelector("button.log"));
-		username.sendKeys("Elie");
-		password.sendKeys("zer");
+		username.sendKeys("Sonic");
+		password.sendKeys("gottagofast");
+		loginBtn.click();
+		
+		Thread.sleep(2000);
+	}
+
+	@Test
+	@Order(11)
+	void manageRest() throws InterruptedException {
+		WebElement restname = driver.findElement(By.id("name"));
+		WebElement address  = driver.findElement(By.id("address"));
+		restname.sendKeys("Sonic Drive In");
+		address.sendKeys("361 Suncrest Towne Centre Drive, Morgantown, WV 26505");
+		WebElement dropdown = driver.findElement(By.className("dropdown-btn"));
+		dropdown.click();
+		Thread.sleep(1000);
+
+		List<WebElement> allCheckboxes = driver.findElements(By.className("multiselect-item-checkbox"));
+		WebElement AmericanCheckbox = allCheckboxes.get(3);
+		AmericanCheckbox.click();
+		dropdown.click();
+		
+		WebElement submitBtn = driver.findElement(By.cssSelector("button.log"));
+		submitBtn.click();
+		Thread.sleep(2000);
+	}
+
+	@Test
+	@Order(12)
+	void addChiliDog() throws InterruptedException {
+		WebElement item = driver.findElement(By.id("item"));
+		WebElement cost = driver.findElement(By.id("cost"));
+		WebElement cals = driver.findElement(By.id("cal"));
+		
+		item.sendKeys("Chili Dog");
+		cost.sendKeys("5");
+		cals.sendKeys("420");
+
+		WebElement submitBtn = driver.findElement(By.cssSelector("button.log"));
+		submitBtn.click();
+		Thread.sleep(2000);
+		item.clear();
+		cost.clear();
+		cals.clear();
+}
+	
+	@Test
+	@Order(13)
+	void addMozz() throws InterruptedException {
+		WebElement item = driver.findElement(By.id("item"));
+		WebElement cost = driver.findElement(By.id("cost"));
+		WebElement cals = driver.findElement(By.id("cal"));
+		
+		item.sendKeys("Mozzerella Sticks");
+		cost.sendKeys("5");
+		cals.sendKeys("750");
+
+		WebElement submitBtn = driver.findElement(By.cssSelector("button.log"));
+		submitBtn.click();
+		Thread.sleep(2000);
+}
+	
+	@Test
+	@Order(14)
+	void logoutRest() throws InterruptedException {
+		WebElement result = driver.findElement(By.xpath("//span[contains(.,'Logout')]"));
+		result.click();
+		
+		Thread.sleep(1000);
+	}
+
+	
+	@Test
+	@Order(15)
+	void clickUserLogin() throws InterruptedException {
+		WebElement userLoginLink = driver.findElement(By.id("log"));
+		userLoginLink.click();
+		Thread.sleep(1000);
+	}
+
+	@Test
+	@Order(16)
+	void deniedLogin() throws InterruptedException {
+		WebElement username = driver.findElement(By.id("username"));
+		WebElement password = driver.findElement(By.id("password"));
+		username.sendKeys("Feed me");
+		password.sendKeys("seymour");
+		WebElement loginBtn = driver.findElement(By.cssSelector("button.log"));
+		loginBtn.click();
+		Thread.sleep(2000);
+		driver.switchTo().alert().accept();
+		Thread.sleep(1000);
+	}
+	
+//	@Test
+//	@Order(17)
+//	void loginUser() throws InterruptedException {
+//
+//		WebElement username = driver.findElement(By.id("username"));
+//		WebElement password = driver.findElement(By.id("password"));
+//		WebElement loginBtn = driver.findElement(By.cssSelector("button.log"));
+//		username.sendKeys("User");
+//		password.sendKeys("password");
+//		loginBtn.click();
+//		
+//		Thread.sleep(1000);
+//	}
+
+	@Test
+	@Order(18)
+	void clickRegister() throws InterruptedException {
+		WebElement registerBtn = driver.findElement(By.cssSelector("[ng-reflect-router-link='register']"));
+		registerBtn.click();
+		
+		Thread.sleep(2000);
+	}
+	
+	@Test
+	@Order(19)
+	void regUserSarah() throws InterruptedException {
+
+		WebElement username = driver.findElement(By.id("username"));
+		WebElement password = driver.findElement(By.id("password"));
+		WebElement registerBtn = driver.findElement(By.cssSelector("button.log"));
+		username.sendKeys("Sarah");
+		password.sendKeys("shinespark");
+		registerBtn.click();
+
+		Thread.sleep(2000);
+	}
+	
+	@Test
+	@Order(20)
+	void loginSarah() throws InterruptedException {
+
+		WebElement username = driver.findElement(By.id("username"));
+		WebElement password = driver.findElement(By.id("password"));
+		WebElement loginBtn = driver.findElement(By.cssSelector("button.log"));
+		username.sendKeys("Sarah");
+		password.sendKeys("shinespark");
 		loginBtn.click();
 		
 		Thread.sleep(1000);
 	}
 
 	@Test
-	@Order(2)
-	void getMeals500cals20budget() throws InterruptedException {
-		loginUser();
+	@Order(21)
+	void getMeals770cals10budget() throws InterruptedException {
 		WebElement calories = driver.findElement(By.id("calories"));
 		WebElement budget   = driver.findElement(By.id("cost"));
 		WebElement dropdown = driver.findElement(By.id("cuisine"));
-		WebElement submit   = driver.findElement(By.xpath("//*[@id=\"mid\"]/button"));
-		calories.sendKeys("500");
-		budget.sendKeys("20");
-		dropdown.click();
-		Thread.sleep(1000);
-
-		List<WebElement> allCheckboxes = driver.findElements(By.className("multiselect-item-checkbox"));
-		System.out.println(allCheckboxes.size());
-		WebElement AmericanCheckbox = allCheckboxes.get(3);
-		AmericanCheckbox.click();
-		dropdown.click();
-		Thread.sleep(1000);
+		WebElement submit   = driver.findElement(By.className("log"));
+		calories.sendKeys("770");
+		budget.sendKeys("10");
+//		dropdown.click();
+//		Thread.sleep(1000);
+//
+//		List<WebElement> allCheckboxes = driver.findElements(By.className("multiselect-item-checkbox"));
+//		WebElement AmericanCheckbox = allCheckboxes.get(3);
+//		AmericanCheckbox.click();
+//		dropdown.click(); //Close dropdown
+//		Thread.sleep(1000);
 		submit.click();
+		
+		Thread.sleep(2000);
+	}
+
+	@Test
+	@Order(22)
+	void selectItem() throws InterruptedException {
+		WebElement result = driver.findElement(By.xpath("//td[contains(.,'Chili Dog')]"));
+		result.click();
 		
 		Thread.sleep(4000);
 	}
-// Click preferences
-// Sign up for a manager
 
 //	@Test
-	@Order(3)
-	void loginManager() throws InterruptedException {
-		driver.get("http://localhost:4200/managerlogin");
-
-		WebElement username = driver.findElement(By.id("username"));
-		WebElement password = driver.findElement(By.id("password"));
-		WebElement loginBtn = driver.findElement(By.cssSelector("button.log"));
-		username.sendKeys("y");
-		password.sendKeys("z");
-		loginBtn.click();
-
-		Thread.sleep(12000);
+//	@Order(23)
+//	void clickLogout() throws InterruptedException {
+//		WebElement logout = driver.findElement(By.className("mat-button"));
+//		logout.click();
+//		
+//		Thread.sleep(4000);
+//	}
+		
+	@Test
+	@Order(24)
+	void logoutUser() throws InterruptedException {
+		WebElement result = driver.findElement(By.xpath("//span[contains(.,'Logout')]"));
+		result.click();
+		
+		Thread.sleep(1000);
 	}
 
+	
 	@AfterAll
 	static void closeDriver() {
 		driver.quit();
